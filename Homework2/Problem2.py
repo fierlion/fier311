fe1546165bd9090b0a5db68a5915820dbbe63f9a
Ray F Allan <fierlion@gmail.com>
2014-04-09 15:01:11 -0700

    Added file structure

--- Homework2/Problem2.py
+++ Homework2/Problem2.py
@@ -7,6 +7,7 @@
 This script creates the following directories (under your
 home directory or a subdirectory of your home) for a command
 line given '-t' [term] and '-c' [class]
+assignments, examples, exams, lecture_notes, submissions
 Example usage:
 python Problem2.py -t spring2014 -c cs311   ...or...
 python Problem2.py --term spring2014 --class cs311
@@ -21,6 +22,19 @@ class Usage(Exception):
     def __init__(self, msg):
         self.msg = msg
 
+class cd:
+    """Context manager for changing the current working directory from
+        'http://stackoverflow.com/questions/431684/how-do-i-cd-in-python'"""
+    def __init__(self, newPath):
+        self.newPath = newPath
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
 def main(argv=None):
 #parse command line options
     if argv is None:
@@ -31,13 +45,15 @@ def main(argv=None):
         except getopt.error, msg:
             raise Usage(msg)
 #process options
+        argList = []
         for o, a in opts:
             if o in ("-h", "--help"):
                 print __doc__
             if o in ("-t", "--term"):
-                mkdir(a)
+                argList.append(a)
             if o in ("-c", "--class"):
-                mkdir(a)
+                argList.append(a)
+        mkdir(argList)
         return 0
 #process arguments
     except Usage, err:
@@ -45,9 +61,22 @@ def main(argv=None):
         print >>sys.stderr, "for help use --help"
         return 2
 
-
-def mkdir(arg):
-    p = subprocess.Popen(["mkdir", arg], stdout=subprocess.PIPE)
+def mkdir(argsIn):
+    if os.path.isdir(argsIn[0]):
+        print "dir " + argsIn[0] + " already exists."
+        return 2
+    subprocess.call(["mkdir", argsIn[0]])
+    with cd(argsIn[0]):
+        if os.path.isdir(argsIn[1]):
+            print "dir " + argsIn[1] + " already exists"
+            return 2
+        subprocess.call(["mkdir", argsIn[1]])
+        with cd(argsIn[1]):
+            subprocess.call(["mkdir", "assigments"])
+            subprocess.call(["mkdir", "examples"])
+            subprocess.call(["mkdir", "exams"])
+            subprocess.call(["mkdir", "lecture_notes"])
+            subprocess.call(["mkdir", "submissions"])
 
 if __name__=="__main__":
     sys.exit(main())
