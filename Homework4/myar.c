34f0383f1a48f17f1fb021c1492112aab3a1e661
Ray F Allan <fierlion@gmail.com>
2014-04-22 16:04:15 -0700

    command line args no leading space between opt and arg bug.  Simply creates out.a file and writes -qinput.txt to this file, overwriting anything aready in the file.

--- Homework4/myar.c
+++ Homework4/myar.c
@@ -12,62 +12,105 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <string.h>
 
 #define BLOCKSIZE 1            //text files have a blocksize of 1
 
 
 int main(int argc, char **argv){
   int c;
+
+  int getopt(int argc, char * const argv[], const char *opstring);
+  extern char *optarg;
+  extern int optind, opterr, optopt;         //disable invalid options
   char *input;
-  char *output;
+  char *output = "out.a";
+
+  void printUse();
+  void openAdd(char* fileOpenAdd, char *fileOut);
   
-  int in_fd;
-  int out_fd;
-
-  char buf[BLOCKSIZE];
-
-  int num_read;
-  int num_written;
-
-  while((c = getopt(argc, argv, "q:x:t:vd:A")) != -1){
+  opterr = 0;
+  while ((c = getopt(argc, argv, "q::x::tvd::A")) != -1)
     switch(c){
-    case 'q':                  //append named files to .a, if no members create empty .a file
-      if (optarg)
-	//handle optarg
-	printf("%s", optarg);
-      else
-	//create empty .a file
-      break;
-    case 'x':                  //extract (copy out) named members, if no arg, extract all
-      if (optarg)
-	//handle optarg
-	printf("%s", optarg);
-      else
-	//extract all
-      break;                   
-    case 't':                  //print a concise table of .a contents
-      break;
-    case 'v':                  //print a verbose table
-      break;
-    case 'd':                  //delete named files from .a, if no arg nothing happens
-      if (optarg)
-	//handle optarg
-	printf("%s", optarg);
-      break;
-    case 'A':                  //append all regular files in current dir, (except .a)
-      break;
-    case '?':
-      if (isprint(optopt))
-	fprintf(stderr, "Unknown option -%c\n", optopt);
-      else
-	fprintf(stderr, "Unknown option character %x\n", optopt);
-      exit(EXIT_FAILURE);
-    default:
-      printf("Unable to get command opts...Exiting");
-      exit(EXIT_FAILURE);
+      case 'q':                  //append named files to .a, if no members create empty .a file
+        if (optarg){
+	  input = optarg;
+	  openAdd(input, output);
+	}
+	else
+	  printf("-q\n");
+        break;
+      case 'x':                  //extract (copy out) named members, if no arg, extract all
+        if (optarg)
+	  printf("-x: %s\n", optarg);
+        else
+	 //extract all
+	  printf("x\n");
+        break;                   
+      case 't':                  //print a concise table of .a contents
+        printf("t\n");      
+        break;
+      case 'v':                  //print a verbose table
+        printf("v\n");
+        break;
+      case 'd':                  //delete named files from .a, if no arg nothing happens
+        if (optarg)
+          printf("-d: %s\n", optarg);
+        break;
+      case 'A':                  //append all regular files in current dir, (except .a)
+        printf("A\n");
+        break;
+      case '?':
+        printf("Unknown option\n");
+        break;
+      default:
+        printUse();
+        exit(EXIT_FAILURE);
     }
 
-
-  }
   return 0;
 }
+
+void printUse(){
+  printf("Useage: myar -q[file.a][files] | -x [file.a][files]\n");
+  printf("        | -tv [file.a] | -d [file.a][files]\n");
+  printf("        | -A [file.a]\n");
+  return;
+}
+
+ void openAdd(char *fileOpenAdd, char *fileOut){
+   char *input = fileOpenAdd;
+   char *output = fileOut;
+   
+   int in_fd;
+   int out_fd;
+
+   char buf[BLOCKSIZE];
+   
+   int num_read;
+   int num_written;
+
+  in_fd = open(input, O_RDONLY);
+  if (in_fd == -1){
+    perror("Can't open input file");
+    exit(EXIT_FAILURE);
+  }
+  
+  out_fd = open(output, O_CREAT | O_WRONLY, 0666);
+  if (out_fd == -1){
+    perror("Can't open/create output file");
+    exit(EXIT_FAILURE);
+  }
+  
+  while((num_read = read(in_fd, buf, BLOCKSIZE)) > 0){
+    num_written = write(out_fd, buf, BLOCKSIZE);
+
+    if (num_written != num_read){
+      perror("error writing file");
+      unlink(output);
+      exit(EXIT_FAILURE);
+    }
+  }
+  return;
+}
+  
