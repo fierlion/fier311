627ba45ce8f42d8c24901bf5d192b44a13f1162f
fierlion <fierlion@gmail.com>
2014-05-09 16:03:33 -0700

    term 3 week 6 commit

---
+++ Homework5/pipes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+
+
+
+  exit(EXIT_SUCCESS);
+}
