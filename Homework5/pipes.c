6cc15d87c5e6b6f86253f51afdef6db879e3a3c4
fierlion <fierlion@gmail.com>
2014-05-11 13:44:23 -0700

    one long pipe

--- Homework5/pipes.c
+++ Homework5/pipes.c
@@ -1,65 +1,84 @@
+#include <sys/wait.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv){
-  int aPipe[2];
-  int bPipe[2];
-
-  pipe(aPipe);
-  pipe(bPipe);
+  int pipes[4];
+  pipe(pipes);
+  pipe(pipes + 2);
 
   switch(fork()){
   case -1:
     perror("fork");
     exit(EXIT_FAILURE);
   case 0:
-    close(aPipe[0]);
-    dup2(aPipe[1], STDOUT_FILENO);
-    close(aPipe[1]);
+    // replace cat's stdout with write part of 1st pipe
+    dup2(pipes[1], 1);
+    // close all pipes (very important!); end we're using was safely copied
+    close(pipes[0]);
+    close(pipes[1]);
+    close(pipes[2]);
+    close(pipes[3]);
     execlp("rev", "rev", (char *)0);
-  default:
-    break;
-  }
-  
-  switch(fork()){
-  case -1:
-    perror("fork3");
+    perror("rev fail exec");
     exit(EXIT_FAILURE);
-  case 0:
-    close(bPipe[0]);
-    dup2(bPipe[1], STDOUT_FILENO);
-    close(bPipe[1]);
-    execlp("sort", "sort", (char *)0);
   default:
     break;
   }
 
   switch(fork()){
   case -1:
-    perror("fork 4");
+    perror("fork2");
     exit(EXIT_FAILURE);
   case 0:
-    close(bPipe[1]);
-    dup2(bPipe[0], STDIN_FILENO);
-    close(bPipe[1]);
-    execlp("cat", "cat", (char *)0);
+    // replace grep's stdin with read end of 1st pipe      
+    dup2(pipes[0], 0);
+    // replace grep's stdout with write end of 2nd pipe
+    dup2(pipes[3], 1);
+    // close all ends of pipes
+    close(pipes[0]);
+    close(pipes[1]);
+    close(pipes[2]);
+    close(pipes[3]);
+    execlp("sort", "sort", (char *)0);
+    perror("sort fail exec");
+    exit(EXIT_FAILURE);
   default:
     break;
   }
-  
-  close(aPipe[0]);
-  //  close(aPipe[1]);
-  close(bPipe[0]);
-  close(bPipe[1]);
+
+
+  switch(fork()){
+  case -1:
+    perror("fork3");
+    exit(EXIT_FAILURE);
+  case 0:
+  // replace cut's stdin with input read of 2nd pipe
+  dup2(pipes[2], 0);
+  // close all ends of pipes
+  close(pipes[0]);
+  close(pipes[1]);
+  close(pipes[2]);
+  close(pipes[3]);
+  execlp("uniq", "uniq", "-c", (char *)0);
+  perror("uniq fail exec");
+  exit(EXIT_FAILURE);
+  default:
+    break;
+  }
+
+  close(pipes[0]);
+  close(pipes[1]);
+  close(pipes[2]);
+  close(pipes[3]);
+
   wait(NULL);
   wait(NULL);
   wait(NULL);
-  //  wait(NULL);
 
   exit(EXIT_SUCCESS);
 }
